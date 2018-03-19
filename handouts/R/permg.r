6101e825e8f80901f88a95e9f2c13986c2e95cae
chl <chl@aliquote.org>
2018-03-19 12:08:34 +0100

    Refactor R code + add TODO list

--- handouts/R/permg.r
+++ handouts/R/permg.r
@@ -1,30 +1,30 @@
-permg<-function(x,y,alpha=.05,est=mean,nboot=1000){
-#
-# Do a two-sample permutation test based on means or any
-# other measure of location or scale indicated by the
-# argument est.
-#
-# The default number of permutations is nboot=1000
-#
-x<-x[!is.na(x)]
-y<-y[!is.na(y)]
-xx<-c(x,y)
-dif<-est(x)-est(y)
-vec<-c(1:length(xx))
-v1<-length(x)+1
-difb<-NA
-temp2<-NA
-for(i in 1:nboot){
-data <- sample(xx, size = length(xx), replace = FALSE)
-temp1<-est(data[c(1:length(x))])
-temp2<-est(data[c(v1:length(xx))])
-difb[i]<-temp1-temp2
-}
-difb<-sort(difb)
-icl<-floor((alpha/2)*nboot+.5)
-icu<-floor((1-alpha/2)*nboot+.5)
-reject<-"no"
-if(dif>=difb[icu] || dif <=difb[icl])reject<-"yes"
-list(dif=dif,lower=difb[icl],upper=difb[icu],reject=reject)
+permg <- function(x, y, alpha = .05, est = mean, nboot = 1000) {
+  ##
+  ## Do a two-sample permutation test based on means or any
+  ## other measure of location or scale indicated by the
+  ## argument est.
+  ##
+  ## The default number of permutations is nboot=1000
+  ##
+  x <- x[!is.na(x)]
+  y <- y[!is.na(y)]
+  xx <- c(x,y)
+  dif <- est(x)-est(y)
+  vec <- c(1:length(xx))
+  v1 <- length(x)+1
+  difb <- NA
+  temp2 <- NA
+  for (i in 1:nboot) {
+    data <- sample(xx, size = length(xx), replace = FALSE)
+    temp1 <- est(data[c(1:length(x))])
+    temp2 <- est(data[c(v1:length(xx))])
+    difb[i] <- temp1 - temp2
+  }
+  difb <- sort(difb)
+  icl <- floor((alpha/2) * nboot + .5)
+  icu <- floor((1-alpha/2) * nboot + .5)
+  reject <- "no"
+  if (dif >= difb[icu] || dif <= difb[icl]) reject<-"yes"
+  list(dif = dif, lower = difb[icl], upper = difb[icu], reject = reject)
 }
 
--- handouts/R/signt.r
+++ handouts/R/signt.r
@@ -1,32 +1,32 @@
-signt<-function(x,y=NULL,alpha=.05,AC=TRUE,SD=FALSE){
-#
-#  Do a sign test on data in x and y
-#  If y=NA, assume x is a matrix with
-#  two columns or has list mode.
-#
-#  Returns n, the original sample size
-#  N, number of paired observations that are not equal to one another.
-#  phat, an estimate of p, the probability that x<y.
-#  ci, a confidence interval for p
-#
-#  The Agresti-Coull method is used by default.
-#  AC=F, Pratt's method is used instead.
-#  SD=TRUE, the Schilling-Doi method is used, which returns a confidence
-#           interval but no p-value.
-#
-if(is.null(y[1])){
-if(ncol(as.matrix(x))!=2)stop('y is null so x should be a matrix or data frame with two columns')
-if(is.matrix(x)||is.data.frame(x))dif<-x[,1]-x[,2]
-if(is.list(x))dif<-x[[1]]-x[[2]]
-}
-if(!is.null(y[1]))dif<-x-y
-dif=elimna(dif)
-n<-length(dif)
-dif<-dif[dif!=0]  # Remove any zero values.
-flag<-(dif<0)
-if(!AC)temp<-binomcipv(y=flag,alpha=alpha)
-if(AC)temp<-acbinomcipv(y=flag,alpha=alpha)
-if(SD)temp=binomLCO(y=flag,alpha=alpha)
-list(Prob_x_less_than_y=temp$phat,ci=temp$ci,n=n,N=length(flag),p.value=temp$p.value)
+signt <- function(x, y = NULL, alpha = .05, AC = TRUE, SD = FALSE) {
+  ##
+  ##  Do a sign test on data in x and y
+  ##  If y=NA, assume x is a matrix with
+  ##  two columns or has list mode.
+  ##
+  ##  Returns n, the original sample size
+  ##  N, number of paired observations that are not equal to one another.
+  ##  phat, an estimate of p, the probability that x<y.
+  ##  ci, a confidence interval for p
+  ##
+  ##  The Agresti-Coull method is used by default.
+  ##  AC=F, Pratt's method is used instead.
+  ##  SD=TRUE, the Schilling-Doi method is used, which returns a confidence
+  ##           interval but no p-value.
+  ##
+  if (is.null(y[1])) {
+    if (ncol(as.matrix(x)) != 2) stop('y is null so x should be a matrix or data frame with two columns')
+    if (is.matrix(x) || is.data.frame(x)) dif <- x[,1] - x[,2]
+    if (is.list(x)) dif <- x[[1]] - x[[2]]
+  }
+  if (!is.null(y[1])) dif <- x - y
+  n <- length(dif)
+  dif <- dif[dif != 0] 
+  flag <- (dif < 0)
+  if (!AC) temp <- binomcipv(y = flag, alpha = alpha)
+  if (AC) temp <- acbinomcipv(y = flag, alpha = alpha)
+  if (SD) temp <- binomLCO(y = flag, alpha = alpha)
+  list(Prob_x_less_than_y = temp$phat, ci = temp$ci,
+       n = n, N = length(flag), p.value = temp$p.value)
 }
 
